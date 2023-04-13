6a0f99665de78bb2e3777fb78af5f766886a1c30
root <root@DESKTOP-K3OKK6C>
2023-04-13 06:30:59 +0100

    bubble sort -> improved version written in python

--- python_scripts/bubble_sort.py
+++ python_scripts/bubble_sort.py
@@ -4,13 +4,12 @@ unsorted_array = [2,8,1,9,3,5,1,3,7,6,2,9]
 def bubble_sort(A):
     x = len(A)
     for i in range(len(A)):
-        for j in range(0,x): 
+        for j in range(0,len(A)): 
             if j!=len(A)-1 and A[j+1] < A[j]:
                 tmp = A[j]
                 A[j] = A[j+1]
                 A[j+1] = tmp
         x-=1
-            
 
     return A
 
