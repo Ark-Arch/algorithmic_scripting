dc7e796155f42f2c8f26e658638c1775d29c2eb3
root <root@DESKTOP-K3OKK6C>
2023-04-13 06:29:12 +0100

    bubble sort -> an algorithmic script written in python

---
+++ python_scripts/bubble_sort.py
@@ -0,0 +1,21 @@
+
+unsorted_array = [2,8,1,9,3,5,1,3,7,6,2,9]
+
+def bubble_sort(A):
+    x = len(A)
+    for i in range(len(A)):
+        for j in range(0,x): 
+            if j!=len(A)-1 and A[j+1] < A[j]:
+                tmp = A[j]
+                A[j] = A[j+1]
+                A[j+1] = tmp
+        x-=1
+            
+
+    return A
+
+sorted_array = bubble_sort(unsorted_array)
+
+print(sorted_array)
+
+
