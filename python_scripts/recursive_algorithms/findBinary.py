de6782a6284412f1617d62c8880d3ab16741b6d1
David Agbemuko <davidagbemuko.ad@gmail.com>
2024-08-13 16:38:09 +0000

    ëditted the find binary algorithm

--- python_scripts/recursive_algorithms/findBinary.py
+++ python_scripts/recursive_algorithms/findBinary.py
@@ -1,15 +1,18 @@
-def findBinary(number):
-
+def findBinary(number, track = False):
     # base case
-    if number == 0:
-        return ''
+    if (number == 0):
+        if track:
+            return ''
+        else:
+            return '0'
     else:
+        track = True
         if number % 2 != 0:
             rem = '1'
         elif number % 2 == 0:
             rem = '0'
             
-        return findBinary(number//2) + rem
+        return findBinary(number//2, track) + rem
 
 
 print(findBinary(233)) # == 1 0 1
